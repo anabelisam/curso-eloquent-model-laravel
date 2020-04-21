0d290eb532841805c0389f04fd0b4f5de4113a77
Ana Belisa <ana.belisa9403>
2020-04-21 01:31:05 -0500

    Add function to insert a new project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Project;
 
 class ProjectController
@@ -9,5 +7,18 @@ class ProjectController
   public function getAllProjects() {
     $projects = Project::all();
     return $projects;
+  }
+
+  public function insertProject() {
+    $project = new Project;
+    $project->city_id = 1;
+    $project->company_id = 1;
+    $project->user_id = 1;
+    $project->name = 'Nombre del proyecto';
+    $project->execution_date = '2020-04-30';
+    $project->is_active = 1;
+    $project->save();
+
+    return "Guardado";
   }
 }
